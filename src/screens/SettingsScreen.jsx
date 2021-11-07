d91d20c72d51cdf91cab237debf366d50120012a
Timothy Goon <t.kgoonx@gmail.com>
2021-11-07 14:42:18 -0500

    Add styles to Settings screen.

--- src/screens/SettingsScreen.jsx
+++ src/screens/SettingsScreen.jsx
@@ -16,6 +16,8 @@ const SettingsScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        backgroundColor: 'white',
         paddingVertical: 20,
         paddingHorizontal: 10
     },
