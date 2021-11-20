34ed4d1cebbf35fd901271d1d231b50563b11b85
Timothy Goon <t.kgoonx@gmail.com>
2021-11-20 18:01:18 -0500

    Add snapshots. Remove dead testing code. Also install react navigation lib for later testing.

--- __tests__/screens/AddPostScreen.test.js
+++ __tests__/screens/AddPostScreen.test.js
@@ -4,67 +4,10 @@ import { render } from '@testing-library/react-native';
 import AddPostScreen from '../../src/screens/AddPostScreen';
 
 describe('<AddPostScreen/>', () => {
-    beforeEach(async () => {
-        global.WebSocket = WebSocket;
-    });
-
     it('has i child', async () => {
         const tree = render(<AddPostScreen />).toJSON();
-        // console.log(tree.children[0].children[0].children[1]);
         expect(tree.children.length).toBe(2);
     });
-
-    // it('can send message', async () => {
-
-    //     const { findByText, findByPlaceholderText } = render(
-    //         <ContextProvider>
-    //             <AddPostScreen />
-    //         </ContextProvider>);
-
-
-
-    //     SecureStore.getItemAsync.mockResolvedValue("token");
-
-    //     await server.connected;
-    //     console.log(await server.connected);
-
-
-
-    //     fireEvent(await findByPlaceholderText('message'), 'onChangeText', 'hello');
-
-
-    //     fireEvent(await findByText('Post'), 'onPress');
-
-    //     console.log(server);
-
-    //     // client.send('hello');
-
-    //     // console.log(tree);
-    //     // let tree;
-    //     // act(() => {
-    //     //     tree = renderer.create(
-    //     //         <ContextProvider>
-    //     //             <AddPostScreen />
-    //     //         </ContextProvider>).toJSON();
-    //     //     console.log(tree);
-    //     //     console.log(tree.children[0].children[0].children[1]);
-    //     //     tree.children[0].children[0].children[1].props.onChangeText('hello');
-    //     // tree.children[1].props.onClick();
-
-
-    //     // const screen = shallow(
-    //     //     <ContextProvider>
-    //     //         <AddPostScreen />
-    //     //     </ContextProvider>);
-    //     // console.log(screen.find("*"));
-    //     // screen.find(ButtonPrim).simulate('click');
-    //     // screen.find(ButtonPrim).simulate('textChange', 'hello');
-
-    //     // });
-
-    //     await expect(server).toReceiveMessage("hello");
-    //     expect(server).toHaveReceivedMessages(["hello"]);
-    // });
 
     it('renders correctly', async () => {
         const tree = render(<AddPostScreen />
@@ -72,6 +15,5 @@ describe('<AddPostScreen/>', () => {
 
         expect(tree).toMatchSnapshot();
     });
-
 
 });
