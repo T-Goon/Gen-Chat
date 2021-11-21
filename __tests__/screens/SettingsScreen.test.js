ef50d53d6cef344f3788fa1224322668acb73392
Timothy Goon <t.kgoonx@gmail.com>
2021-11-21 13:21:15 -0500

    Add test for the Settings Screen.

--- __tests__/screens/SettingsScreen.test.js
+++ __tests__/screens/SettingsScreen.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, fireEvent } from '@testing-library/react-native';
+import * as SecureStore from 'expo-secure-store';
 
 import SettingsScreen from '../../src/screens/SettingsScreen';
+jest.mock('expo-secure-store');
 
 describe('<SettingsScreen/>', () => {
     it('has i child', async () => {
@@ -14,5 +16,15 @@ describe('<SettingsScreen/>', () => {
         const tree = render(<SettingsScreen />).toJSON();
 
         expect(tree).toMatchSnapshot();
+    });
+
+    it('Logs out', async () => {
+        const navivation = { reset: jest.fn()};
+        const settingScreen = render(<SettingsScreen navigation={navivation} />);
+
+        await fireEvent(settingScreen.getByText('Logout'), 'press');
+
+        expect(navivation.reset).toHaveBeenCalledWith({ index: 1, routes: [{ name: 'Login' }] });
+        expect(SecureStore.deleteItemAsync).toHaveBeenCalledWith('token');
     });
 });
